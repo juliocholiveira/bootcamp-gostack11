c3b19519648cc6438d6a2836d3e1deb93d4469ff
Júlio César <juliocholiveira@gmail.com>
2020-05-26 08:08:29 -0300

    estiliza a página de repositórios

--- nivel03-a-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ nivel03-a-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronsRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +12,53 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository - {params.repository}</h1>;
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronsLeft size={16} /> voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>dsjflksd fkljsd flkjs dflk dfs k</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link key={1} to={`/repository/{repo.full_name}`}>
+          <div>
+            <strong>jflkdsjfklsd</strong>
+            <p>dlfjklds j</p>
+          </div>
+          <FiChevronsRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
--- nivel03-a-primeiro-projeto-react/src/routes/index.tsx
+++ nivel03-a-primeiro-projeto-react/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
+    <Route exact path="/">
+      <Redirect to="/dashboard" />
+    </Route>
+    <Route path="/dashboard" exact component={Dashboard} />
     <Route path="/repository/:repository+" component={Repository} />
   </Switch>
 );
